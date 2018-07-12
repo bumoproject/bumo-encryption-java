e7eead1b336f7d249941f235b7ce80b6edb34581
riven <frm430342210@163.com>
2018-07-12 15:06:46 +0800

    Delete exception

--- src/src/main/java/io/bumo/encryption/utils/hash/HashUtil.java
+++ src/src/main/java/io/bumo/encryption/utils/hash/HashUtil.java
@@ -4,11 +4,9 @@ public class HashUtil {
 	/**
 	 * generate hex string of hash
 	 * @param src
-	 * @param type 0(SHA256) or 1(SM3)
 	 * @return hex string of hash
-	 * @throws Exception 
 	 */
-	public static String GenerateHashHex(byte[] src) throws Exception {
+	public static String GenerateHashHex(byte[] src) {
 		Sha256 sha256 = new Sha256(src);
 		byte[] hash = sha256.finish();
 		return io.bumo.encryption.utils.hex.HexFormat.byteToHex(hash).toLowerCase();
